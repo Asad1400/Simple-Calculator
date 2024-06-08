f04b6b84b0dc9eda1357a2824b7d63ed05fde165
Asad1400 <147860328+Asad1400@users.noreply.github.com>
2024-06-09 01:16:22 +0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter First Number",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: "Enter Second Number",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message: "Select one of the operators to perforM action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(`The sum of ${answer.firstNumber} and ${answer.secondNumber} is ${answer.firstNumber + answer.secondNumber}`);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(`The Difference of ${answer.firstNumber} and ${answer.secondNumber} is ${answer.firstNumber - answer.secondNumber}`);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(`The Product of ${answer.firstNumber} and ${answer.secondNumber} is ${answer.firstNumber * answer.secondNumber}`);
+}
+else if (answer.operator === "Division") {
+    console.log(`The Quotient of ${answer.firstNumber} and ${answer.secondNumber} is ${answer.firstNumber / answer.secondNumber}`);
+}
+else {
+    console.log("Invalid Operator");
+}
+console.log(answer);
